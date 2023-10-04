643d861004992a9e8eb72fdb3c36eedf225e4f06
Davide Carraro <124809373+Daveeeee13@users.noreply.github.com>
2023-10-04 09:20:05 +0200

    v0.2
    Sistemazione tabella

--- js/javascript.js
+++ js/javascript.js
@@ -1,35 +1,34 @@
 function creaTabella(n1, n2){
-    /*var tabella = document.getElementById("TABLE");
-    x.setAttribute("id", "myTable");
-    document.body.appendChild(tabella); */
+    
+
 
     var x = document.createElement("TABLE");
     x.setAttribute("id", "myTable");
     document.body.appendChild(x);
     
+    x.style.border = "1px solid";
+    x.style.borderCollapse="collapse";
+    
     
 
     for(i=0; i<n1; i++){
 
-        /*var y = document.createElement("tr");
-        y.setAttribute("id", "myTr");
-        tabella.appendChild(y);*/
 
         var y = document.createElement("TR");
         y.setAttribute("id", "myTr"+i);
         document.getElementById("myTable").appendChild(y);
-        var t=document.createTextNode("prova");
+        var t=document.createTextNode("");
         y.appendChild(t);
         
 
         for(j=0; j<n2; j++){
-            /*var z = document.createElement("td");
-            var t = document.createTextNode("cell");
-            y.appendChild(t);
-            tabella.appendChild(z);*/
             
             var z = document.createElement("TD");
-            var t = document.createTextNode("prova");
+            var t = document.createTextNode(" ");   //esto sulle caselle
+            z.style.border="1px solid";
+            z.style.height="20px";
+            z.style.width="20px";
+            
             z.appendChild(t);
             document.getElementById("myTr"+i).appendChild(z);
             
