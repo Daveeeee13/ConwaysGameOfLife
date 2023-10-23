87ba9dc22a0b9e590cc4c7f2f28233eaf0eb7ef9
Davide Carraro <124809373+Daveeeee13@users.noreply.github.com>
2023-10-23 09:10:08 +0200

    v0.9
    Vari metodi controllo cellule a scuola

--- js/javascript.js
+++ js/javascript.js
@@ -22,22 +22,29 @@ function creaForm(){
     bottoneinvio.setAttribute("onclick", "creaTabella()");
     bottoneinvio.innerHTML = "Invio";
 
+    //bottone gioca
     var bottonegioca=document.createElement("button");
     bottonegioca.setAttribute("id", "Gioca");
     bottonegioca.setAttribute("onclick", "gioca()");
     bottonegioca.innerHTML = "Gioca";
-    
+
+    //contenitore div
+    var contenitore=document.createElement("div");
+    contenitore.setAttribute("id", "contenitore");
+    //contenitore.setAttribute("marginTop", );
+
     
     //appendo tutto sulla pagina
     document.body.appendChild(f);
     f.appendChild(input);
     document.body.appendChild(bottoneinvio);
     document.body.appendChild(bottonegioca);
+    document.body.appendChild(contenitore);
     
 }
 
 
-function creaTabella(n1){
+function creaTabella(){
 
     numerodicelle=document.getElementById("ncelle").value;
     if(numerodicelle<20 || numerodicelle>100){
@@ -46,11 +53,12 @@ function creaTabella(n1){
 
     x = document.createElement("TABLE");
     x.setAttribute("id", "myTable");
-    document.body.appendChild(x);
+    document.getElementById("contenitore").appendChild(x);
+    
     
     x.style.border = "1px solid";
     x.style.borderCollapse="collapse";
-    x.width="100%";
+    //x.width="100%";
     
 
 
@@ -75,8 +83,8 @@ function creaTabella(n1){
             z.setAttribute("onclick", "clickcella(this);")
 
             z.style.border="1px solid";
-            z.style.height="20px";
-            z.style.width="20px";
+            z.style.height="1em";
+            z.style.width="1em";
             z.style.backgroundColor='white';
                         
             z.appendChild(t);
@@ -141,14 +149,36 @@ function stampa_matrice(){
 
 }
 
+function verifica_cella(r, c){
+    var cont=0;
+    for(i=r-1; i<=r+1; i++){
+        for(j=c-1; j<=c+1; j++){
+            if (!(i==r && j==c)&&(i<0)&&(j<0)&&(j>=numerodicelle)&&(i>=numerodicelle)){
+                if(m[i][j]==1){ //conto solo se la cella è viva
+                    cont++;
+                }
+            }
+        }
+    }
+    return cont;
+    
+}
 
+function controlla_matrice(){
+    for(i=0; i<numerodicelle; i++){
+        for(j=0; j<numerodicelle; j++){
+            conteggio=verifica_cella(i, j);
+            console.log("m["+i+","+j+"] = "+conteggio);
+        }
+    }
+}
 
 function gioca(){
 
    
     inizializza_matrice() //test matrice
     crea_matrice();
-
+    controlla_matrice();
     stampa_matrice();
+    //verifica_cella();
 }
-
